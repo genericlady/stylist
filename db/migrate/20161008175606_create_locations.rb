ae6e8e7e95b461f9ddf4a4ffd91adfa353b80800
genericlady <yiannakokalas@gmail.com>
2016-10-10 16:32:52 -0400

    got rid of address and salon. moved address attributes to location

--- db/migrate/20161008175606_create_locations.rb
+++ db/migrate/20161008175606_create_locations.rb
@@ -1,6 +1,13 @@
 class CreateLocations < ActiveRecord::Migration[5.0]
   def change
     create_table :locations do |t|
+      t.belongs_to :location, index: true
+      t.string :address1, null: false, limit: 90
+      t.string :address2,              limit: 90
+      t.string :city,  null: false, limit: 90
+      t.string :state, null: false, limit: 2
+      t.string :zip,   null: false, limit: 9
+      
       t.timestamps
     end
   end
