10510ac49549e05d2e0425f3fec35f46e0501de2
genericlady <yiannakokalas@gmail.com>
2016-10-10 23:27:31 -0400

    added stylist search in index action

--- app/controllers/stylists_controller.rb
+++ app/controllers/stylists_controller.rb
@@ -1,6 +1,17 @@
 class StylistsController < ApplicationController
   def index
-    @stylists = User.all.limit(10)
+    if params[:stylist] && params[:near]
+      @search_term = params[:stylist] + params[:near]
+      stylist_search_term = StylistSearchTerm.new(@search_term)
+      @stylists =
+        User.joins(:locations)
+            .where(
+              stylist_search_term.where_clause,
+              stylist_search_term.where_args
+            )
+    else
+      @stylists = []
+    end
   end
   
 end
