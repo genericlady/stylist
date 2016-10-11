840da215bd906f2b966a8ca7a2dcfdea6f12aecc
genericlady <yiannakokalas@gmail.com>
2016-10-10 23:27:07 -0400

    removed locatable_id and type

--- db/migrate/20161008175606_create_locations.rb
+++ db/migrate/20161008175606_create_locations.rb
@@ -1,7 +1,7 @@
 class CreateLocations < ActiveRecord::Migration[5.0]
   def change
     create_table :locations do |t|
-      t.belongs_to :location, index: true
+      t.belongs_to :user, index: true
       t.string :address1, null: false, limit: 90
       t.string :address2,              limit: 90
       t.string :city,  null: false, limit: 90
--- db/migrate/20161008194427_add_locatable_to_location.rb
+++
@@ -1,8 +0,0 @@
-class AddLocatableToLocation < ActiveRecord::Migration[5.0]
-  def change
-    add_column :locations, :locatable_id, :integer
-    add_column :locations, :locatable_type, :string
-    
-    add_index :locations, [:locatable_type, :locatable_id]
-  end
-end
