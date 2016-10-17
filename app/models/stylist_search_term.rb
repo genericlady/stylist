125f1ba90c286b459bb1a54249498a43d0600af0
genericlady <yiannakokalas@gmail.com>
2016-10-17 16:01:18 -0400

    cleared out some edge cases for name first, last with city or state search terms

--- app/models/stylist_search_term.rb
+++ app/models/stylist_search_term.rb
@@ -2,22 +2,39 @@ class StylistSearchTerm
   attr_reader :where_clause, :where_args, :order
   
   def initialize(search_terms)
-    @where_clause = ""
-    @where_args = {}
-    @location_terms = ""
-    @name_terms     = ""
-
-    if !search_terms[:stylist].empty? && !search_terms[:near].empty?
-      @location_terms = search_terms[:near]
-      @name_terms     = search_terms[:stylist]
-      build_name_location_search 
+        @where_args = {}
+      @where_clause = ""
+    @location_terms = []
+        @name_terms = ""
+        
+    set_location_terms(search_terms)
+    set_name_terms(search_terms)
+    
+    if @location_terms.length == 2 && !@name_terms.empty?
+      build_name_city_and_state_search 
+    elsif @location_terms.length == 1 && !@name_terms.empty?
+      build_name_city_or_state_search
     elsif !search_terms[:near].empty?
-      @location_terms = search_terms[:near]
       build_location_search
     elsif !search_terms[:stylist].empty?
-      @name_terms = search_terms[:stylist]
       build_name_search
     end
+  end
+  
+  def set_location_terms(search_terms)
+    if !search_terms[:near].empty?
+      @location_terms = scan_near_terms(search_terms[:near])  
+    end
+  end
+  
+  def set_name_terms(search_terms)
+    if !search_terms[:stylist].empty?  
+      @name_terms = search_terms[:stylist]  
+    end
+  end
+  
+  def scan_near_terms(near_terms)
+    near_terms.scan(/(?:\b[\p{L}][\p{L}`-]+\s*)+/io)
   end
   
   private
@@ -30,7 +47,32 @@ class StylistSearchTerm
     @order = "last_name ASC "  
   end
   
-  def build_name_location_search
+  def build_name_city_or_state_search
+    @where_clause << case_insensitive_search(:city)
+    @where_args[:city] = starts_with(city)
+
+    @where_clause << " AND #{case_insensitive_search(:first_name)}"
+    @where_args[:first_name] = starts_with(first_name)
+    
+    @where_clause << " AND #{case_insensitive_search(:last_name)}"
+    @where_args[:last_name] = starts_with(last_name)
+    
+    @where_clause << " OR #{case_insensitive_search(:state)}"
+    @where_args[:state] = starts_with(state)
+      
+    @where_clause << " AND #{case_insensitive_search(:first_name)}"
+    @where_args[:first_name] = starts_with(first_name)
+    
+    @where_clause << " AND #{case_insensitive_search(:last_name)}"
+    @where_args[:last_name] = starts_with(last_name)
+    
+    @order = "city = " +
+      ActiveRecord::Base.connection.quote(city) +
+      " asc, last_name asc"
+  
+  end
+  
+  def build_name_city_and_state_search
     @where_clause << case_insensitive_search(:city)
     @where_args[:city] = starts_with(city)
 
@@ -53,12 +95,12 @@ class StylistSearchTerm
     @where_clause << case_insensitive_search(:city)
     @where_args[:city] = starts_with(city)
 
-    @where_clause << " AND #{case_insensitive_search(:state)}"
+    @where_clause << " OR #{case_insensitive_search(:state)}"
     @where_args[:state] = starts_with(state)
  
     @order = "city = " +
       ActiveRecord::Base.connection.quote(city) +
-      " desc, last_name asc"
+      " DESC, last_name ASC"
   end
   
   def starts_with(keyword)
@@ -70,15 +112,11 @@ class StylistSearchTerm
   end
   
   def city
-    @location_terms.split(', ').first
+    @location_terms.first
   end
   
   def state
-    if (terms = @location_terms.split(', ')).length > 1
-      terms.last
-    else
-      ""
-    end
+    @location_terms.last
   end
   
   def first_name
