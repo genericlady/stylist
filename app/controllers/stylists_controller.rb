5810ef6351cb3d3b09ff097b0e233dd571a2c192
genericlady <yiannakokalas@gmail.com>
2016-10-17 21:17:40 -0400

    removed comments

--- app/controllers/stylists_controller.rb
+++ app/controllers/stylists_controller.rb
@@ -2,12 +2,7 @@ class StylistsController < ApplicationController
   def index
     if params[:search_terms].present?
       @search_terms = params[:search_terms]
-      # stylist_search_sql = StylistSearchSQL.new(@search_terms)
       @stylists = StylistSearch.resolve(@search_terms)
-        # User.joins(:locations)
-        #     .where(stylist_search_sql.where_clause,
-        #            stylist_search_sql.where_args)
-        #     .order(stylist_search_sql.order)
     else
       @stylists = []
     end
