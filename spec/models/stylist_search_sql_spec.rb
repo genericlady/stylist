551a8b315dd9918e2fce902f21b0b08b5d6460f8
genericlady <yiannakokalas@gmail.com>
2016-10-17 21:46:17 -0400

    added initialization specs

---
+++ spec/models/stylist_search_sql_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+describe StylistSearchSQL do
+  def stylist_where(stylist_search_sql)
+    User.joins(:locations)
+        .where(stylist_search_sql.where_clause,
+               stylist_search_sql.where_args)
+        .order(stylist_search_sql.order)  
+  end
+  
+  it "Can be initialized" do
+    search_terms = { stylist: "foo", near: "new york, NY" }
+    stylist_search_sql = StylistSearchSQL.new search_terms
+    expect(stylist_search_sql.class).to eq(StylistSearchSQL)
+  end
+end
