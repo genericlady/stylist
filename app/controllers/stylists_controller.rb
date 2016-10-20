8c539626778a42203e5187acd372ecf49c74af4c
genericlady <yiannakokalas@gmail.com>
2016-10-20 16:22:52 -0400

    added pagination

--- app/controllers/stylists_controller.rb
+++ app/controllers/stylists_controller.rb
@@ -1,8 +1,12 @@
 class StylistsController < ApplicationController
+  PAGE_SIZE = 10
+
   def index
+    @page = (params[:page] || 0).to_i
+
     if params[:search_terms].present?
       @search_terms = params[:search_terms]
-      @stylists = StylistSearch.resolve(@search_terms)
+      @stylists = StylistSearch.resolve(@search_terms).limit(PAGE_SIZE)
     else
       @stylists = []
     end
