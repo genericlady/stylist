a1a39ace92c3aa9749cb1b8aab355fa35bfea5de
genericlady <yiannakokalas@gmail.com>
2016-10-20 16:22:15 -0400

    added route for stylists

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users, controllers: { sessions: 'users/sessions' }
 
   get 'stylists/index', to: 'stylists#index'
+  get 'stylists',       to: 'stylists#index'
   
   root 'dashboard#index'
 end
