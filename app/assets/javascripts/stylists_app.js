7f9c1c3da19dd16257ffb435b86068f66c853c7a
genericlady <yiannakokalas@gmail.com>
2016-10-23 13:05:55 -0400

    added controller and http get request

---
+++ app/assets/javascripts/stylists_app.js
@@ -0,0 +1,28 @@
+app = angular.module('stylists',[]);
+
+app.controller("StylistSearchController", [
+          "$scope", "$http",
+  function($scope ,  $http) {
+    $scope.stylists = [];
+    $scope.search = function(params) {
+      $http.get("/stylists.json",
+                {
+                  paramSerializer: '$httpParamSerializerJQLike',
+                  "params": {
+                    "search_terms": {
+                      'stylist': params['search_terms']['stylist'],
+                      'near':    params['search_terms']['near']
+                    }
+                  }
+                }
+      ).then(function(response) {
+        $scope.stylists = response.data;
+      },function(response) {
+        alert("There was a problem: " + response.status);
+        }
+      );
+    $scope.searchedFor = params['search_terms']['stylist'] + ' ' +
+                         params['search_terms']['near']
+    };
+  }
+]);
