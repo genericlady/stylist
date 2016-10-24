1be820420219d92b97cc36e20ddcf3a36bab01f1
genericlady <yiannakokalas@gmail.com>
2016-10-24 02:27:21 -0400

    added methods for pagination

--- app/models/stylist_search.rb
+++ app/models/stylist_search.rb
@@ -1,10 +1,12 @@
 class StylistSearch
-  def self.resolve(search_terms)
+  def self.resolve(search_terms, page_size, page)
     stylist_search_sql = StylistSearchSQL.new(search_terms)
 
     User.joins(:locations)
         .where(stylist_search_sql.where_clause,
                stylist_search_sql.where_args)
-        .order(stylist_search_sql.order)  
+        .order(stylist_search_sql.order)
+        .offset(page_size * page)
+        .limit(page_size)
   end
 end
