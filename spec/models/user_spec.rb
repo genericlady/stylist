95d9d919c2428baf57c999bdf692ba03cfadff60
genericlady <yiannakokalas@gmail.com>
2016-10-25 14:30:47 -0400

    database constraint matcher

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,26 @@
+require 'rails_helper'
+require 'support/violate_check_constraint_matcher'
+
+describe User do
+  describe "username" do
+    let(:user) {
+      User.create!(email: "foo@example.com",
+                   password: "qwertyuiop",
+                   password_confirmation: "qwertyuiop",
+                   username: "default")
+    }
+
+    it "absolutely prevents duplicate usernames for users" do
+      User.create!(email: "moreFoo@example.com",
+                   password: "1234567890",
+                   password_confirmation: "1234567890",
+                   username: "foobar")
+
+      expect {
+        user.update_attribute(:username, "foobar")
+      }.to violate_check_constraint(:index_users_on_username)
+
+    end
+  end
+end
+
