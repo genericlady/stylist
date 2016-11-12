04fcf6788d5e59d32a39e79d44c1bd1520d78998
genericlady <yiannakokalas@gmail.com>
2016-11-11 23:19:38 -0500

    added a pg_search_scope for :search_by_full_name

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,12 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   has_many :locations
+
+  include PgSearch
+
+  pg_search_scope :search_by_full_name,
+                  :against => [:first_name, :last_name],
+                  :using   => {
+                    :tsearch => { :prefix => true }
+                  }
 end
