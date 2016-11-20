2cef1bf363a2027757dae0ccb13f27479aaf89d0
genericlady <yiannakokalas@gmail.com>
2016-11-20 12:26:00 -0500

    Abstract the StylistsSearchController from stylists_app.js into it's own folder and file

---
+++ app/assets/javascripts/stylists/controllers/StylistSearchController.js
@@ -0,0 +1,51 @@
+app.controller("StylistSearchController", [
+          "$scope", "$http",
+  function($scope ,  $http) {
+    var page = 0;
+
+    $scope.searchTerms = { "search_terms": "" };
+
+    $scope.stylists = [];
+
+    $scope.search = function(search_params) {
+      if (search_params.search_terms.length < 3) {
+        return;
+      }
+
+      $scope.searchTerms = search_params['search_terms'];
+
+      $http.get("/stylists.json",
+                {
+                  paramSerializer: '$httpParamSerializerJQLike',
+                  "params": {
+                    "search_terms": $scope.searchTerms,
+                    "page": page
+                  }
+                }
+      ).then(function(response) {
+        $scope.stylists = response.data;
+      },function(response) {
+        alert("There was a problem: " + response.status);
+        }
+      );
+
+      $scope.searchedFor = search_params['search_terms'];
+
+    }
+
+    $scope.previousPage = function() {
+      if (page > 0) {
+        page = page - 1;
+        $scope.search({ "search_terms": $scope.searchTerms });
+      };
+    }
+
+    $scope.nextPage = function() {
+      page = page + 1;
+      $scope.search({ "search_terms": $scope.searchTerms });
+    }
+  }
+    
+]);
+
+
