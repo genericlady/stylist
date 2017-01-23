4dac34aced9f6c46ad9a777fe6ab0a88b9db62cd
yianna <yianna@thoughtbot.com>
2017-01-23 16:47:45 -0500

    Add some logic to map markers to convert an entire collection of results to either service markers or stylist markers

--- app/controllers/stylists_controller.rb
+++
@@ -1,3 +0,0 @@
-class StylistsController < ApplicationController
-
-end
--- app/models/map_marker.rb
+++ app/models/map_marker.rb
@@ -1,11 +1,38 @@
 class MapMarker
+  def self.to_markers(results, query)
+    case query[:type]
+    when /stylist/i
+      create_stylist_markers(results, query)
+    when /service/i
+      create_service_markers(results, query)
+    end
+  end
+
+  def self.create_stylist_markers(results, query)
+    if query.nil? || query[:location].empty?
+     MapMarker.new_for_each_stylist(results)
+    else
+     location_expression = LocationExpression.create(query)
+     MapMarker.new_for_each_stylist(results, location_expression)
+    end
+  end
+
+  def self.create_service_markers(results, query)
+    if query.nil? || query[:location].empty?
+     MapMarker.new_for_each(@results)
+    else
+     location_expression = LocationExpression.create(query)
+     MapMarker.new_for_each(@results, location_expression)
+    end
+  end
+
 
   def self.new_for_each_stylist(stylists, expression = nil)
     stylists.reduce([]) do |markers, stylist|
-      stylist["locations"].each do |l|
+      stylist.locations.each do |l|
         location = Location.new(l)
         if expression.nil? || location.full_address.match(expression)
-          label = stylist["first_name"][0..1]
+          label = stylist.full_name_string[0..1]
           coords =
             {
               lat: location.latitude,
@@ -17,7 +44,6 @@ class MapMarker
       markers
     end
   end
-
 
   def self.new_for_each(results, expression = nil)
     results.reduce([]) do |markers, result|
