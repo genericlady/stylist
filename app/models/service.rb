f79cf28ec3b7220700448cc7f53355450eb50beb
yianna <yianna@thoughtbot.com>
2017-01-28 21:18:46 -0500

    Add pg_search functionality to service

--- app/models/service.rb
+++ app/models/service.rb
@@ -1,5 +1,25 @@
 class Service < ActiveRecord::Base
+  include PgSearch
+
   belongs_to :user
+  has_many :locations, through: :user
   validates :name, :description, :price, presence: true
 
+  pg_search_scope :search_by_name_and_description,
+                   against: [:name, :description],
+                   using: {
+                     tsearch: { prefix: true } 
+                   },
+                  associated_against: {
+                    locations: [:city, :state],
+                   }
+
+  def self.search(query)
+    return top20 if query.to_s.empty?
+    search_by_name_and_description(query.to_s)
+  end
+
+  def self.top20
+    Service.all.limit(20)
+  end
 end
