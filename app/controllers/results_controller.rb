fbb9e4a316819b7870e2dbc2c16c31a5331ee485
yianna <yianna@thoughtbot.com>
2017-01-29 12:50:25 -0500

    fixup whitespace in results controller

--- app/controllers/results_controller.rb
+++ app/controllers/results_controller.rb
@@ -1,9 +1,9 @@
 class ResultsController < ApplicationController
   def index
     @results =
-        Search.
-        new(query).
-        results
+      Search.
+      new(query).
+      results
 
     @markers = MapMarker.create_service_markers(@results, query)
   end
