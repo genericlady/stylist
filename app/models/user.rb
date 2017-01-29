cf5d8fbbefd1818f25e4a716c5e113e767e26e08
yianna <yianna@thoughtbot.com>
2017-01-28 21:19:22 -0500

    Add a create locations method for user

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,28 +5,14 @@ class User < ApplicationRecord
   has_many :services
   accepts_nested_attributes_for :locations
 
-  include PgSearch
-
-  pg_search_scope :search_by_name,
-                  :against => [:first_name, :last_name],
-                  :using   => {
-                    :tsearch => { :prefix => true }
-                  }
-
- pg_search_scope :search_by_name_and_location,
-                   against: [:first_name, :last_name],
-                   using: {
-                     tsearch: { prefix: true } 
-                   },
-                   associated_against: {
-                     locations: [:city, :state]
-                   }
-
-
   def print_locations
     cities_and_states = locations.map do |l|
       l.city + " " + l.state
     end
     cities_and_states.join(' | ')
+  end
+
+  def create_service(service)
+    self.services.create(service)
   end
 end
