a8992b30559dc30397ebf1a9320abbe4ab1a9dc6
yianna <yianna@thoughtbot.com>
2017-02-01 19:09:43 -0500

    Add service card presenter to presenters and add a controller for services

---
+++ app/presenters/service_card_presenter.rb
@@ -0,0 +1,67 @@
+class ServiceCardPresenter < BasePresenter
+  presents :service
+  delegate :description, :price, :name, to: :service
+
+  def print_description
+    %Q(<p>#{description}</p>).html_safe
+  end
+
+  def print_price
+    %Q(<p>#{price}</p>).html_safe
+  end
+
+  def print_rating
+    %Q(
+        <span class="icon icon-star"></span>
+        <span class="icon icon-star"></span>
+        <span class="icon icon-star"></span>
+        <span class="icon icon-star"></span>
+        <span class="icon icon-star"></span>
+      ).html_safe
+  end
+
+  def print_name
+    %Q(<h3 id="thumbnail-label">#{name}</h3>).html_safe
+  end
+
+  def print_total_votes
+    %Q(<p>Total Votes: 498</p>).html_safe
+  end
+
+  def buttons
+    if owner?
+      %Q(<p>#{button_to_edit && button_to_delete}
+           <a href="#" class="btn btn-danger" role="button">Delete</a>
+         </p>).html_safe
+    else
+      service_details_button
+      %Q(<p>#{service_details_button}</p>).html_safe
+    end
+  end
+
+  def link_to_reviews
+  end
+
+  private
+  def service_details_button
+    class_styles = "btn btn-primary"
+    options = { class: class_styles, role: "button" }
+    path = user_service_path(service)
+
+    link_to('Details', path, options)
+  end
+
+  def button_to_edit
+  end
+
+  def button_to_delete
+    class_styles = "btn btn-primary"
+    options = { class: class_styles, role: "button", method: "delete" }
+
+    link_to "Delete", path, options
+  end
+
+  def owner?
+    current_user.id == service.user_id
+  end
+end
