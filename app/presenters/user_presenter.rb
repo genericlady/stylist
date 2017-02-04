16c6174a5cc5e60c9028a0293a2006c21236e962
yianna <yianna@thoughtbot.com>
2017-02-03 20:24:23 -0500

    Add some extra behavior for presenting a user on the root page

--- app/presenters/user_presenter.rb
+++ app/presenters/user_presenter.rb
@@ -8,22 +8,65 @@ class UserPresenter < BasePresenter
   end
 
   def avatar
-    default_avatar_path = "../assets/#{avatar_name}"
     %Q(<img src="#{default_avatar_path}" class="rounded-circle media-object">).
       html_safe
+  end
+
+  def default_avatar_path
+    "../assets/#{avatar_name}"
+  end
+
+  def card_profile_image
+    %Q(<img class="card-profile-img" src="#{default_avatar_path}">).html_safe
   end
 
   def print_name
     %Q(<h3 class="profile-header-user">#{full_name}</h3>).html_safe
   end
 
-  def bio
+  def bio(class_name: nil)
     bio_text = <<-BIO
       I wish i was a little bit taller, 
       wish i was a baller, wish i had a girl… also.
     BIO
 
-    %Q(<p class="profile-header-bio">#{bio_text}</p>).html_safe
+    if class_name.nil?
+      %Q(<p>#{bio_text}</p>).html_safe
+    else
+      %Q(<p class="#{class_name}">#{bio_text}</p>).html_safe
+    end
+  end
+
+  def link_to_show
+    %Q(<a class="text-inherit" href="/">#{full_name}</a>).html_safe
+  end
+
+  def photo_thumbnails(&block)
+    10.times do
+      yield create_thumbnail
+    end
+  end
+
+  def create_thumbnail
+    thumbnail_styles = <<-THUMB
+      margin-bottom: 10px;
+      margin-right: 10px;
+      display: inline-block;
+      vertical-align: bottom;
+    THUMB
+
+    image_attributes = <<-IMAGE
+      data-width="640"
+      data-height="640"
+      data-action="zoom"
+      style="width: 84px; height: 85px;"
+    IMAGE
+
+    %Q(
+        <div style="#{thumbnail_styles}">
+          <img src="/assets/haircut-emoji.png" #{image_attributes}>
+        </div>
+    ).html_safe
   end
 
   private
