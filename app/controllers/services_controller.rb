6053576e2ba20fd8dda7ab2f2345ce59ed71136f
yianna <yianna@thoughtbot.com>
2017-02-05 10:37:54 -0500

    Add crud actions to services controller

--- app/controllers/services_controller.rb
+++ app/controllers/services_controller.rb
@@ -2,4 +2,33 @@ class ServicesController < ApplicationController
   def show
     @service = Service.find params[:id]
   end
+
+  def edit
+    @service = Service.find params[:id]
+  end
+
+  def new
+  end
+
+  def update
+    @service = current_user.services.find params[:id]
+    @service.update (service_params)
+    flash[:notice] = "Your service has been updated successfully"
+    redirect_to user_service_path @service
+  end
+
+  def create
+    @service = current_user.services.create(service_params)
+
+    if @service.save
+      redirect_to user_service_path current_user, @service
+    else
+      render 'new'
+    end
+  end
+
+  private
+  def service_params
+    params.require(:service).permit(:id, :name, :description, :price)
+  end
 end
