a9570754066ffda93a38e079a7c5866368fedd1f
yianna <yianna@thoughtbot.com>
2017-02-28 12:26:01 -0500

    Test sign up process for successfully and failing to make a new account

--- spec/features/sign_up_process_spec.rb
+++ spec/features/sign_up_process_spec.rb
@@ -1,41 +1,34 @@
-require 'rails_helper'
+require_relative '../rails_helper'
 
 feature "Signup process", :type => :feature do
 
-  it "signs me up for an account to provide beauty" do
+  scenario "signs me up for an account" do
     visit '/users/sign_up'
     within("#new_user") do
-      fill_in 'First name', with: 'Susie'
-      fill_in 'Last name',  with: 'Salamander'
+      fill_in 'First name', with: 'Sally'
+      fill_in 'Last name', with: 'bojangles'
       fill_in 'Email',      with: 'user@example.com'
-      fill_in 'Address1',   with: '10 sheridan square'
-      fill_in 'City',       with: 'new york'
-      fill_in 'State',      with: 'NY'
-      fill_in 'Zip',        with: '10014'
       fill_in 'Password',   with: '1234567890'
       fill_in 'Password confirmation', with: '1234567890'
+      fill_in 'Zipcode', with: '11106'
     end
 
     click_button 'Sign up'
-    expect(page).to have_content 'You have signed up successfully.'
+    expect(page).to have_content 'Welcome! You have signed up successfully.'
   end
 
-  # it "Signing up for an account with a bad address will fail validation" do
-  #   visit '/users/sign_up'
-  #   within("#new_user") do
-  #     fill_in 'First name', with: 'Susie'
-  #     fill_in 'Last name',  with: 'Salamander'
-  #     fill_in 'Email',      with: 'user@example.com'
-  #     fill_in 'Address1',   with: '666 Satan Ave'
-  #     fill_in 'City',       with: 'new york'
-  #     fill_in 'State',      with: 'NY'
-  #     fill_in 'Zip',        with: '10014'
-  #     fill_in 'Password',   with: '234567890'
-  #     fill_in 'Password confirmation', with: '1234567890'
-  #   end
+  scenario "Signing up for an account with a bad address will fail validation" do
+    visit '/users/sign_up'
 
-  #   click_button 'Sign up'
-  #   #expect(page).to have_content ''
-  # end
+    click_button 'Sign up'
+
+    expect(page).to have_content "Email can't be blank"
+    expect(page).to have_content "Password can't be blank"
+    expect(page).to have_content "First name can't be blank"
+    expect(page).to have_content "Last name can't be blank"
+    expect(page).to have_content "Zipcode can't be blank"
+    expect(page).to have_content "Zipcode is too short (minimum is 5 characters)"
+    expect(page).to have_content "Zipcode is invalid"
+  end
 
 end
