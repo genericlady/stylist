6d362c78d2837ad622749c43875451ed248f59ea
yianna <yianna@thoughtbot.com>
2017-03-03 09:14:37 -0500

    Add new license instantiation for user in new, edit and update actions in providers controller.

--- app/controllers/providers_controller.rb
+++ app/controllers/providers_controller.rb
@@ -1,4 +1,23 @@
 class ProvidersController < ApplicationController
   def new
+    @user = current_user
+    @license = @user.licenses.new
   end
+
+  def edit
+    @user = User.find(params[:id])
+    @license = @user.licenses.new
+  end
+
+  def update
+    @user = User.find(params[:id])
+    @license = @user.licenses.new
+    binding.pry
+    if @user.update_attributes(user_params)
+      render 'new'
+    else
+      render 'edit'
+    end
+  end
+
 end
