fff70f0cb91478f5a76173ae8fe426641c24556f
yianna <yianna@thoughtbot.com>
2017-03-03 09:20:26 -0500

    Add s3 credentials to application_record model for use across many models.

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,10 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
+
+  def s3_credentials
+    template = ERB.new File.new("config/s3.yml.erb").read
+    s3 = YAML.load template.result(binding)
+    s3[Rails.env]
+  end
+
 end
