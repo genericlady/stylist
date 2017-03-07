2df701999e6140d1395fec502e143dfb90320e6a
yianna <yianna@thoughtbot.com>
2017-03-07 16:25:26 -0500

    Comment out verify_authorized before hook to reuse in another branch of policy check

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   include Pundit
   protect_from_forgery with: :exception
-  after_action :verify_authorized
+  # after_action :verify_authorized
   before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
 
