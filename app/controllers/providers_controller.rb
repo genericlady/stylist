b05cdd67f96d251a204fd1f7e4693592d835ed01
yianna <yianna@thoughtbot.com>
2017-03-07 16:10:31 -0500

    Add authorize @user and redirects with flash notices to providers controller. Also, add a new definition of get_user

--- app/controllers/providers_controller.rb
+++ app/controllers/providers_controller.rb
@@ -2,6 +2,7 @@ class ProvidersController < ApplicationController
   def new
     @user = current_user
     @license = @user.licenses.new
+    authorize @license
   end
 
   def edit
@@ -11,10 +12,14 @@ class ProvidersController < ApplicationController
 
   def update
     @user = get_user
-    @license = @user.licenses.new
     @user.update_attributes(user_params)
-    create_user_license
-    render 'new'
+    authorize @user
+    redirect_to new_provider_path, flash: { success: "User has been updated." }
+  end
+
+  private
+  def get_user
+    User.find(params[:id])
   end
 
 end
